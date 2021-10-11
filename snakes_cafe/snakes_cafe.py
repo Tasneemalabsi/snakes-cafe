52a320d8579ca4cbfc0b40564ee7728cb4d520b8
Tasneemalabsi <tasneem.alabsi@gmail.com>
2021-10-12 00:16:08 +0300

    finish

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -35,31 +35,30 @@ Unicorn Tears
 ***********************************
 """)
 menu = {
-    'wings' : 0,
-    'cookies' : 0,
-    'spring rolls' : 0,
-    'salmon' : 0,
-    'steak' : 0,
-    'meat tornado' : 0,
-    'a literal garden' : 0,
-    'ice cream' : 0,
-    'cake' : 0,
-    'pie' : 0,
-    'coffee' : 0,
-    'tea' : 0,
-    'unicorn tears' : 0
+    'wings': 0,
+    'cookies': 0,
+    'spring rolls': 0,
+    'salmon': 0,
+    'steak': 0,
+    'meat tornado': 0,
+    'a literal garden': 0,
+    'ice cream': 0,
+    'cake': 0,
+    'pie': 0,
+    'coffee': 0,
+    'tea': 0,
+    'unicorn tears': 0
 }
 
 order = input("what's your order ?  ")
 
 while order != "quit":
-    
+
     if order in menu:
+        
         menu[order] += 1
-        for i in menu:
-            if menu[order] >= 1:
-                print(f'**{menu[order]} order of {order} have been added to your meal **')
+        print(f'**{menu[order]} order of {order} have been added to your meal **')
     else:
         print("your order is not in the menu, order again please ")
-        order= input("what's your order ?  ")
+    order = input("what's your order ?  ")
 
